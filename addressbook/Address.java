7ee77a33c443a26750c2f1743b8cb2da71d9da55
Jagannath888606 <48204376+Jagannath888606@users.noreply.github.com>
2019-03-29 19:12:13 +0530

    address

---
+++ addressbook/Address.java
@@ -0,0 +1,70 @@
+package com.bridgeit.oops.addressbook;
+
+public class Address 
+{
+
+	private String locality;
+	private String city;
+	private String state;
+	private String zip;
+
+	public Address()
+	{
+
+	}
+
+	public Address(String locality, String city, String state, String zip) 
+	{
+		super();
+		this.locality = locality;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Address [locality=" + locality + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+
+	public String getLocality()
+	{
+		return locality;
+	}
+
+	public void setLocality(String locality)
+	{
+		this.locality = locality;
+	}
+
+	public String getCity() 
+	{
+		return city;
+	}
+
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+
+	public String getState() 
+	{
+		return state;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+	public String getZip() 
+	{
+		return zip;
+	}
+
+	public void setZip(String zip) 
+	{
+		this.zip = zip;
+	}
+}
